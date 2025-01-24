504da35edeeb2437cc384f2bde583b36f4cdc636
westcrime <westcrime77@rambler.ru>
2025-01-24 22:59:29 +0300

    added bad request exception

---
+++ DotaMaster/DotaMaster.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotaMaster.Domain.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+            : base("Bad request.")
+        {
+        }
+
+        public BadRequestException(string message)
+            : base(message)
+        {
+        }
+
+        public BadRequestException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
